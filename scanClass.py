44a5c42d44c7112ce21b1b7386d8966721c55f37
18151101@st.hcmute.edu.vn <18151101@st.hcmute.edu.vn>
2023-02-26 21:05:52 +0700

    new server scan

---
+++ scanClass.py
@@ -0,0 +1,116 @@
+from enum import unique
+from math import floor
+import cv2
+import numpy as np
+from skimage.feature import peak_local_max
+from skimage.segmentation import watershed
+from scipy import ndimage
+from collections import Counter
+import matplotlib.pyplot as plt
+import sys
+from PIL import Image, ImageDraw
+
+from sklearn.cluster import KMeans
+import websockets
+
+
+np.set_printoptions(threshold = sys.maxsize)
+
+class ScanClass:        
+
+    def DetectAndTrimDisk(self,Img ,padding_size):
+        Imgresize = cv2.resize(Img, (600, 410))
+        minRadius     = 0
+        maxRadius     = Imgresize.shape[1] #image width
+        minDist       = 100
+        biggestRadius = 0 
+        biggestCenter = (0,0)
+        gray    = cv2.cvtColor(Imgresize, cv2.COLOR_BGR2GRAY)
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, minDist = minDist,
+            minRadius= minRadius,maxRadius= maxRadius) 
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0,:]:
+                if i[2] > biggestRadius:
+                    biggestRadius = i[2]
+                    biggestCenter = (i[0], i[1])
+
+        mask = np.zeros_like(Imgresize)
+        cv2.circle(mask, biggestCenter, biggestRadius - padding_size,
+            (255,255,255), -1)
+
+        roi = cv2.bitwise_and(Imgresize, mask)
+        return roi
+
+
+    def LoadImage(self, ImgUrl):
+        imgOriginal = cv2.imread(ImgUrl)
+     
+        return imgOriginal
+
+
+    def RoiAndGray(self, roi):
+        roiandgray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
+
+        return roiandgray
+
+    
+    
+    def ThreshImage(self,grayandroi,ThreshValue):
+        thresh = cv2.threshold(grayandroi, ThreshValue, 255,  
+        cv2.THRESH_BINARY_INV )[1]
+
+        return thresh 
+
+    
+    def LocalImage(self,threshImg, minDistanceValue):
+        distance_map = ndimage.distance_transform_edt(threshImg)
+        local_max    = peak_local_max(distance_map, indices=False,
+            min_distance=minDistanceValue)
+        markers = ndimage.label(local_max, structure=np.ones((3,3)))[0]
+        labelsImg  = watershed(-distance_map, markers, mask=threshImg)
+
+        return labelsImg
+    
+    
+    def CountColoni(self,labelsImg,roiandgray,roi):
+        MIN_BACTERIA_SIZE = 20
+        countBacteria    = 0
+        bacteriaColonies = []
+        bacteriaCenters  = []
+
+        uniqueLabels = np.unique(labelsImg)
+        for label in np.unique(labelsImg):
+            if label == 0:
+                continue
+
+            mask = np.zeros(roiandgray.shape, dtype="uint8")
+            mask[labelsImg == label] = 255
+
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+            biggestContourInAzone = max(cnts, key=cv2.contourArea)
+
+            currentContourSize = cv2.contourArea(biggestContourInAzone)
+
+            if currentContourSize < MIN_BACTERIA_SIZE:
+                continue
+
+            centerOfContour = cv2.moments(biggestContourInAzone)
+            centerX = int(centerOfContour['m10']/centerOfContour['m00'])
+            centerY = int(centerOfContour['m01']/centerOfContour['m00'])
+            centerOfBacteria = (centerX,centerY)
+            bacteriaCenters.append([centerX,centerY])
+
+            countBacteria += 1
+
+            bacteriaObject = { "bacteria id: ":  countBacteria, " size ": currentContourSize, " position ": centerOfBacteria}
+            showed = ("bacteria id: " + str(countBacteria) + " size: " + str(currentContourSize) + " position: " + str(centerOfBacteria) )
+            bacteriaColonies.append(bacteriaObject)
+            
+            finalmage = cv2.putText(roi,str(countBacteria), centerOfBacteria, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,255), 1)
+            ContoursColonies = cv2.drawContours(finalmage,[biggestContourInAzone], -1, (255,0,0), 1)
+
+        return finalmage, showed, countBacteria    
+    
--- scanClass_server.py
+++ scanClass_server.py
@@ -6,53 +6,119 @@ import numpy as np
 import websockets
 import asyncio
 
-tam = False
 
-ScanObject = ScanClass()
-# img = ScanObject.LoadImage(r"C:\Users\admin\Desktop\do_an_scan\Image file\real_2.png")
+ScanObject= ScanClass()
+# img = ScanObject.LoadImage(r"C:\Users\admin\Desktop\do_an_scan\Image file\real_5.png")
 # roi = ScanObject.DetectAndTrimDisk(img,20)
 # roiandgray = ScanObject.RoiAndGray(roi)
-# threshImg = ScanObject.ThreshImage(roiandgray,131)
-# labelsImg = ScanObject.LocalImage(threshImg,3)
-# FinalImage, bacteriaColonies, total = ScanObject.CountColoni(labelsImg,roiandgray,roi)
-# #cv2.imshow("final", FinalImage)
-# # print(bacteriaColonies)
-# # cv2.waitKey(0)
-# print(total)
+# threshImg = ScanObject.ThreshImage(roiandgray,100)
+# labelsImg = ScanObject.LocalImage(threshImg,10)
+# finalImage, bacteriaColonies, total = ScanObject.CountColoni(labelsImg,roiandgray,roi)
+# cv2.imshow("final image", finalImage)
+# cv2.waitKey(0)
+#cv2.imshow("final1", threshImg1)
+# print(bacteriaColonies)
+# cv2.waitKey(0)
 # cv2.waitKey(0)
 
 clients = set()  #set = *array ma phan tu khong trung nhau 
+
+def thresholdImage(image,threshValue):
+    roiandgray = ScanObject.RoiAndGray(image)
+    threshImg = ScanObject.ThreshImage(roiandgray,threshValue)
+
+
+    return threshImg
+
+    
+def distanceImage(image,threshValue, dinstaceValue):
+    roiandgray = ScanObject.RoiAndGray(image)
+    threshImg  = ScanClass.ThreshImage(roiandgray,threshValue)
+    labelsImg = ScanObject.LocalImage(threshImg,dinstaceValue)   
+    finalImage = ScanObject.CountColoni(labelsImg,roiandgray,image)
+
+    return finalImage
+
+def countColonies(image,threshValue, distanceValue):
+    roiandgray = ScanObject.RoiAndGray(image)
+    threshImg = ScanObject.ThreshImage(roiandgray,threshValue)
+    labelsImg = ScanObject.LocalImage(threshImg,distanceValue)
+
+    total = ScanObject.CountColoni(labelsImg,roiandgray,image)
+
+    return total
 
 async def processData(message, websocket):
 
     if message == 'cmdimg':
         imgbuffer = await websocket.recv()
-        print("asdasd")
         numpyArray = np.frombuffer(imgbuffer, np.uint8) #mang so cua numpy
-        imageConverter = cv2.imdecode(numpyArray, cv2.IMREAD_COLOR) #MAT
-        cv2.imwrite(r'C:\Users\admin\Desktop\do_an_scan\Source_work_image\SourceImage.jpg', imageConverter)
-        roi = ScanObject.DetectAndTrimDisk(imageConverter,20)
-        roiandgray = ScanObject.RoiAndGray(roi)
-        threshImg = ScanObject.ThreshImage(roiandgray,131)
-        labelsImg = ScanObject.LocalImage(threshImg,3)
-        FinalImage, bacteriaColonies, total = ScanObject.CountColoni(labelsImg,roiandgray,roi)
+        image = cv2.imdecode(numpyArray, cv2.IMREAD_COLOR) #MAT
+        
+        finalImage,_,_ = countColonies(image)
        
-        _, img_buf_arr = cv2.imencode(".jpg", FinalImage)
-        cv2.imwrite("asd.jpg",img_buf_arr)
-        byte_img = img_buf_arr.tobytes()
-        print(len(byte_img))
-        await websocket.send(imgbuffer)
-        tam = True
+        _, img_buf_arr = cv2.imencode(".png", finalImage)
+        cv2.imwrite("asd.jpg",finalImage)
+
+        imageBufferToArray = img_buf_arr.tobytes()
+
+        print(len(imageBufferToArray))
+        
+        await websocket.send(imageBufferToArray)
+
         return
-    
-    if tam == False:
-        return
-    
+
     if message == 'cmdcount':
+        imgbuffer = await websocket.recv()
+        numpyArray = np.frombuffer(imgbuffer, np.uint8) #mang so cua numpy
+        image = cv2.imdecode(numpyArray, cv2.IMREAD_COLOR)
+        _,_,total = countColonies(image)
         await websocket.send(total)
+
+        return
+    
+    #cmdthresh_60
+    if 'cmdthresh_' in message:
+       threshStr = message.split("_")
+       threshValue = int(threshStr[1])
+
+       imgbuffer = await websocket.recv()
+       numpyArray = np.frombuffer(imgbuffer, np.uint8) #mang so cua numpy
+       image = cv2.imdecode(numpyArray, cv2.IMREAD_COLOR) #MAT
+
+       imgThresh = thresholdImage(image, threshValue)
+       _, img_buf_arr = cv2.imencode(".png", imgThresh)
+       
+       cv2.imwrite('thresh.png', imgThresh)
+
+       imageBufferToArray = img_buf_arr.tobytes()
+
+       print(len(imageBufferToArray))
+        
+       await websocket.send(imageBufferToArray)
+
+       return
+
+    if "cmdsens_" in message:
+        SensStr = message.split("_")
+        threshValue = int(SensStr[1])
+        SensValue   = int(SensStr[2])
+        
+        imgbuffer = await websocket.recv()
+        numpyArray = np.frombuffer(imgbuffer, cv2.IMREAD_COLOR)
+        image = cv2.imdecode(numpyArray,cv2.IMREAD_COLOR)
+        imgSens = distanceImage(image,threshValue,SensValue)
+        _,img_buf_arr = cv2.imencode(".png", imgSens)
+        cv2.imwrite("distance.png",imgSens)
+        imageBufferToArray = img_buf_arr.tobytes()
+        await websocket.send(imageBufferToArray)
+
+        return
+
      
 
-
+#738054 - 737280 = 774
+#
                                                                            
 
 
